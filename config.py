ebf73f8fbcc5dee5d6c72c9af6fe3dc4de0def14
Shahdoost <154603779+wwwTHMwww@users.noreply.github.com>
2026-06-08 12:40:36 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# تنظیمات NetiShield
+
+SOURCES = [
+    "https://sub.shadowproxy66.workers.dev/sub/be80a76c-6044-417c-9bff-e587f9380d05",
+    "https://sub.shadowproxy66.workers.dev/sub/214abfe9-791e-44ba-bd8e-0afe298119f6",
+    "https://sub.shadowproxy66.workers.dev/sub/99066519-727d-44d0-b65a-6034f21bf3a2",
+    "https://sub.shadowproxy66.workers.dev/sub/b2bc17e9-fd87-44ef-84b2-394d9763d6a6"
+]
+
+CHANNEL_NAME = "NetiShield"
+FINAL_TAG = "#🔥NetiShield"
+TOTAL_CONFIGS = 600
+OUTPUT_FILE = "sub.txt"
+TIMEOUT = 30
+MAX_RETRIES = 2
