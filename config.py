29047a68dbbd849f071bbec836f648f61414304d
Shahdoost <154603779+wwwTHMwww@users.noreply.github.com>
2026-07-06 16:49:20 +0330

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ SOURCES = [
     "https://sub.shadowproxy66.workers.dev/sub/be80a76c-6044-417c-9bff-e587f9380d05#ShadowProxy66(1)",
     "https://sub.shadowproxy66.workers.dev/sub/99066519-727d-44d0-b65a-6034f21bf3a2#ShadowProxy66(2)",
     "https://sub.shadowproxy66.workers.dev/sub/7429c429-a848-4ef6-8508-4b23fd0472de#ShadowProxy66(3)",
-    "https://sub.shadowproxy66.workers.dev/sub/104ea085-c4e8-498a-9988-f2d6acf2e070#ShadowProxy66(4)",
-    "https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/refs/heads/main/ss_configs.txt"
+    "https://sub.shadowproxy66.workers.dev/sub/104ea085-c4e8-498a-9988-f2d6acf2e070#ShadowProxy66(4)"
 ]
 
 CHANNEL_NAME = "NetiShield"
